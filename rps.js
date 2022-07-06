b2188ad38ace4a742ae760f38193adf298a09d4c
rhnsct <rhnsct@gmail.com>
2022-07-06 11:06:06 +0200

    make function with clicking on choices

--- rps.js
+++ rps.js
@@ -1,13 +1,17 @@
 const options = ['rock', 'paper', 'scissors'];
 const possibleHands = ['rock>scissors', 'scissors>paper', 'paper>rock']
 
+let computerScore = 0;
+let playerScore = 0;
+
 function computerPlay(){
     let choice = options[Math.floor(Math.random()*options.length)];
     return choice
 };
 
+
 function playerSelection(){
-    let choice = prompt('Choose your hand rock/paper/scissors!').toLowerCase();
+    let choice = replyClick();
     if (options.includes(choice)) {
         return choice
     } else {
@@ -17,7 +21,7 @@ function playerSelection(){
     }
 };
 
-function playRound(computerPlay, playerSelection){
+function round(computerPlay, playerSelection){
     let compete = `${computerPlay}>${playerSelection}`
     if (computerPlay == playerSelection){
         alert("It's a draw!")
@@ -28,36 +32,72 @@ function playRound(computerPlay, playerSelection){
     } else {
         alert(`Player wins! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerPlay}.`)
         return 2
-    }
+    };
 };
 
-function game(){
-    let computerScore = 0
-    let playerScore = 0
-    for (let i = 0; i < 5; ++i){
-        let go = playRound(computerPlay(), playerSelection());
-
-        if (go == 0) {
-            --i
-            console.log(go)
-        } else if (go == 1){
-            computerScore += 1
-            console.log(go)
-        } else {
-            playerScore += 1
-            console.log(go)
-        }
-        console.log('Player Score =' + playerScore + ' Computer Score = ' + computerScore)
-    }
-    return declareWinner(computerScore, playerScore)
+function game(clicked_id){
+    play(clicked_id);  
+    
 };
+
+function hideShow() {
+    let choicesDiv = document.getElementById('choices')
+    choicesDiv.setAttribute('style', 'display: none')
+    let endgame = document.getElementById('endgame')
+    endgame.setAttribute('style', 'display: flex')
+};
+
+function showHide() {
+    
+    let choicesDiv = document.getElementById('choices')
+    choicesDiv.setAttribute('style', 'display: flex')
+    let endgame = document.getElementById('endgame')
+    endgame.setAttribute('style', 'display: none')
+
+}
 
 function declareWinner(compScore, playerScore) {
+    
+    hideShow();
+
     if (compScore > playerScore) {
         return "Player loses!"
     } else {
         return "Player wins!"
-    }
+    };
 };
 
-console.log(game())
+
+
+function play(clicked_id) {
+    let go = round(computerPlay(), clicked_id);
+    let computerScoreText = document.getElementById('computerScore');
+    let playerScoreText = document.getElementById('playerScore');
+    
+    if (go == 0) {
+        console.log(go);
+    } else if (go == 1){
+        computerScore += 1
+        computerScoreText.textContent = `${computerScore}`
+        console.log(go);
+    } else {
+        playerScore += 1
+        playerScoreText.textContent = `${playerScore}`
+        console.log(go);
+    };
+    
+    console.log(typeof(playerScore))
+    console.log(playerScore >= 5)
+    if (playerScore >= 5 || computerScore >= 5) {
+        console.log('working')
+        declareWinner(computerScore, playerScore);
+    } else {
+        console.log('test');
+    };
+};
+
+function reset(){
+    playerScore = 0
+    computerScore = 0
+    showHide()
+};
